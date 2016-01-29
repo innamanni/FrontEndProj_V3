288b780449c19750f35dea7a48556c46b15bb8b7
kitdoor <kitdoor@yahoo.com>
2016-01-28 22:48:23 -0500

    1/28

--- js/crud_app.js
+++ js/crud_app.js
@@ -38,11 +38,13 @@
 	
 	function ReadController($http){
 		var vm = this;
-		$http.get('http://localhost/BackEndProj_V3/readAddrController.php').
+		$http.get('http://localhost/BackEndProj_V3/readPersonController.php').
 	    success(function(data) {
 	    	vm.userData = data.result;
-	    	console.log("vm.userData:");
-			console.log(vm.userData);
+	    	console.log("data:");
+			console.log(data);
+			console.log("vm.userData[0].phoneDTO:");
+			console.log(vm.userData[0].phoneDTO);
 	    	});
 	};
 	
@@ -83,34 +85,75 @@
 	};
 	
 	angular
+    .module('app')
+    .factory('phoneService', phoneService);
+	
+	function phoneService($resource) {
+	
+		return { 
+			getPhoneTypes: function () {
+				var PhoneTypes = $resource('http://localhost/BackEndProj_V3/getPhoneTypes.php');
+				return PhoneTypes.query(function(phoneTypes){
+					return phoneTypes;
+				});
+			}
+		};
+	}
+
+	
+	angular
+	.module('app')
+	.controller('PhoneController', PhoneController);
+	
+	function PhoneController(phoneService) {
+		var vm = this;
+		vm.phoneTypeList = phoneService.getPhoneTypes();
+		console.log('****PHONE TYPE LIST****:');
+		console.log(vm.phoneTypeList);
+	};
+	
+	angular
 	.module('app')
 	.controller('CreateController', CreateController);
 	
 	function CreateController($http){
 		var vm = this;
 		console.log("I am in CreateController Y'all!!!!");
-		vm.address = {};
-		vm.update = function(address) {
-	    	console.log("update method in CreateController");
-	    	if (address.street2 == undefined) {
-	    		address.street2 = " ";
+		vm.person = {};
+		vm.create = function(person) {
+	    	console.log("create method in CreateController");
+	    	
+	    	if (person.phone.phone_type_id == undefined){
+	    		person.phone.phone_type_id = "1";
 	    	}
-	    	if (address.state == undefined) {
-	    		address.state = "10";
+	    	if (person.address.state_id == undefined){
+	    		person.address.state_id = "10";
+	    	}
+	    	if (person.address.street2 == undefined){
+	    		person.address.street2 = "";
 	    	}
 	    	
-	    	var myData = JSON.stringify(address);
-	    	console.log('address.state: ' + address.state);
-	    	console.log('myData: ' + myData);
+	    	console.log('person.phone.phone_number: ');
+	    	console.log(person.phone.phone_number);
+	    	console.log('person.phone.phone_type_id: ');
+	    	console.log(person.phone.phone_type_id);
+	    	console.log('person.address.state_id: ');
+	    	console.log(person.address.state_id);
+	    	
+	    	var myData = JSON.stringify(person);
+	    	
+	    	console.log('myData: ');
+	    	console.log(myData);
 	    	
 	    	$http({
-	            url: 'http://localhost/BackEndProj_V3/createAddrController.php',
+	            url: 'http://localhost/BackEndProj_V3/createPersonController.php',
 	            method: "POST",
 	            data: myData,
 	            headers: {'Content-Type': 'application/json'}
 	    	}).success(function(response) {
 	    	    console.log('Success!');
-	    	    console.log('response as is from backend: ' + response);
+	    	    console.log('response as is from backend: ');
+	    	    console.log(response);
 	    	    vm.createResponse = response.message;
 	    		console.log('response in scope: ' + vm.createResponse);
 			}).error(function(response) {
@@ -119,14 +162,14 @@
 			});
 	    };
 	
-	    vm.reset = function(address) {
-	    	if (address != undefined){
-		    	//console.log(address);
-		    	address.street1 = "";
-		    	address.street2 = "";
-		    	address.city = "";
-		    	address.state = "";
-		    	address.zip = "";
+	    vm.reset = function(person) {
+	    	if (person != undefined){
+		    	//console.log(person);
+		    	person.lname = "";
+		    	person.fname = "";
+		    	person.email = "";
+		    	person.phone.phone_number = "";
+		    	person.phone.phone_type_id = "1";
 	    	}
 	    };
 	};
@@ -139,49 +182,51 @@
 		var vm = this;
 		vm.idList = [];
 		
-		//display all addresses in the db
+		//display all person in the db
 		//SHOULD CHECK IF THE DATA IS ALREADY IN SCOPE!
-		$http.get('http://localhost/BackEndProj_V3/readAddrController.php').
+		$http.get('http://localhost/BackEndProj_V3/readPersonController.php').
 	    success(function(data) {
+	    	console.log('readPersonController gets data:');
 	    	console.log(data);
 	    	vm.userData = data.result;
 	    });
 		
-		//create an array of address_ids to delete
-		vm.checkDelete = function (address_id){
-			vm.checkExists = function(address_id){
+		//create an array of person_ids to delete
+		vm.checkDelete = function (person_id){
+			console.log('person_id from checkDelete');
+			console.log(person_id);
+			vm.checkExists = function(person_id){
 				vm.inList = false;
 				for (var i = 0; i < vm.idList.length; i++){
-					if (address_id == vm.idList[i]){
+					if (person_id == vm.idList[i]){
 						vm.inList = true;
 						vm.idList.splice(i, 1);
 						break;
 					}
 				}
 				if (vm.inList == false){
-					vm.idList[vm.idList.length] = address_id;
+					vm.idList[vm.idList.length] = person_id;
 				}
 			};
 			
-			vm.checkExists(address_id);
-			//for (var i = 0; i < $scope.idList.length; i++){
-			//	console.log('idList[' + i + ']: ' + $scope.idList[i]);
-			//}
+			vm.checkExists(person_id);
 		};
 		
-		//delete selected addresses from the db
+		//delete selected persons from the db
 		vm.deleteItems = function(){
 			if (vm.idList.length > 0){
 				vm.toObject = function () {
 					var objIdList = {};
 					for (var i = 0; i < vm.idList.length; ++i)
+					{
 						if (vm.idList[i] !== undefined){
 							objIdList[i] = vm.idList[i];
 						}
+					}
 					return objIdList;
 				};
 				
-				var myURL = 'http://localhost/BackEndProj_V3/deleteAddrController.php?deleteId=';
+				var myURL = 'http://localhost/BackEndProj_V3/deletePersonController.php?deleteId=';
 					
 				for (var i=0; i < vm.idList.length; i++){
 					myURL += vm.idList[i];
@@ -212,15 +257,19 @@
 	
 	function UpdateListController($http, $location){
 		var vm = this;
-		//get all addresses from the db
-		$http.get('http://localhost/BackEndProj_V3/readAddrController.php').
+		//get all person from the db
+		$http.get('http://localhost/BackEndProj_V3/readPersonController.php').
 	    success(function(data) {
 	    	console.log(data);
 	    	vm.userData = data.result;
 	    });
 		
 		//set update id
-		vm.setUpdateId = function(address_id){ vm.updateId = address_id; };
+		vm.setUpdateId = function(person_id){
+			console.log('person_id');
+			console.log(person_id);
+			vm.updateId = person_id; 
+		};
 		
 		//load updateRecord template
 		vm.loadNewTemplate = function(){
@@ -235,25 +284,25 @@
 	function UpdateRecordController($http, $location){
 		var vm = this;
 		vm.updateObj = $location.search();
-		vm.address_id = {"address_id" : vm.updateObj.updateId};
-		console.log('vm.address_id is: ');
-		console.log(vm.address_id);
+		vm.person_id = {"person_id" : vm.updateObj.updateId};
+		console.log('vm.person_id is: ');
+		console.log(vm.person_id);
 		
 		//get one record for one id
-		var myData = JSON.stringify(vm.address_id);
-    	console.log('myData: ');
+		var myData = JSON.stringify(vm.person_id);
+    	console.log('***Update Record***myData: ');
     	console.log(myData);
     	
     	$http({
-            url: 'http://localhost/BackEndProj_V3/updateAddrController.php',
+            url: 'http://localhost/BackEndProj_V3/updatePersonController.php',
             method: "POST",
             data: myData,
             headers: {'Content-Type': 'application/json'}
     	}).success(function(response) {
-    	    console.log('Success!');
-    	    vm.userData = response.result;
-    	    console.log('after restful call, response is: ');
+    	    console.log('***response.result from updatePersonController.php***');
     	    console.log(response.result);
+    	    console.log(response.result.phoneDTO);
+    	    vm.userData = response.result;
 		}).error(function(response) {
 			console.log(response);
 			console.log('There was a problem');
@@ -269,19 +318,12 @@
 			console.log('hi from update function, userData is: ');
 			console.log(userData);
 			
-	    	if (vm.userData.street2 == undefined) {
-	    		vm.userData.street2 = " ";
-	    	}
-	    	if (vm.userData.state == undefined) {
-	    		vm.userData.state = "10";
-	    	}
-	    	
 	    	var myData = JSON.stringify(userData);
 	    	console.log('FROM UPDATE METHOD IN UPDATE RECORD CNTRL, myData: ');
 	    	console.log(myData); 
 	    	
 	    	$http({
-	            url: 'http://localhost/BackEndProj_V3/updateAddrRecordController.php',
+	            url: 'http://localhost/BackEndProj_V3/updatePersonRecordController.php',
 	            method: "POST",
 	            data: myData,
 	            headers: {'Content-Type': 'application/json'}
@@ -291,7 +333,7 @@
 	    	    console.log(response);
 	    	    vm.updateResponse = "RECORD UPDATED";
 	    		console.log('response in scope: ');
-	    		console.log(vm.createResponse);
+	    		console.log(vm.updateResponse);
 			}).error(function(response) {
 				console.log(response);
 				console.log('There was a problem');
